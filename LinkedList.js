ea00c063554d2171141a568297f9671560520b87
SaadJhujhJS <saadjhujh8@gmail.com>
2025-04-19 14:02:42 +0500

    format fixes

--- LinkedList.js
+++ LinkedList.js
@@ -1,38 +1,36 @@
-class node{
-    constructor(value){
-    this.value=value;
-    this.next=null;
+class node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
-}
-class linkedlist{
-    constructor(){
-        this.head=null;
-        this.tail=null;
-    }
+class linkedlist {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
 
+  append(value) {
+    const newNode = new node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+  }
 
-append(value){
-    const newNode=new node(value);
-    if(!this.head){
-        this.head=newNode;
-        this.tail=newNode;
+  printLinkedList() {
+    let current = this.head;
+    while (current) {
+      console.log(current.value);
+      current = current.next;
     }
-    else{
-        this.tail.next=newNode;
-        this.tail=newNode;
-    }
-}
-
-printLinkedList(){
-    let current=this.head;
-    while(current){
-        console.log(current.value);
-        current=current.next;
-    }
-}
+  }
 }
 
-const mylist=new linkedlist;
+const mylist = new linkedlist();
 mylist.append(10);
 mylist.append(20);
 mylist.append(30);
